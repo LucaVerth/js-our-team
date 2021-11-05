5bcbb4a801d6afd7a403e096c4ef46e2e14e1d61
LucaVerth <luca.verthuy@gmail.com>
2021-11-05 09:53:43 +0100

    bug fix form img wasn't stored properly // fixed

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-const button = document.getElementById('addMemberButton');
-let inputs = document.querySelectorAll('input');
+const button = document.getElementById("addMemberButton");
+let inputs = document.querySelectorAll("input");
 
 const memberArr = [];
 const givenMemberArr = [
   {
-    name : 'Angela Caroll',
-    role : 'Chief Editor',
-    image : 'img/angela-caroll-chief-editor.jpg'
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    image: "img/angela-caroll-chief-editor.jpg",
   },
   {
-    name : 'Walter Gordon',
-    role : 'Office Manager',
-    image : 'img/walter-gordon-office-manager.jpg'
+    name: "Walter Gordon",
+    role: "Office Manager",
+    image: "img/walter-gordon-office-manager.jpg",
   },
   {
-    name : 'Angela Lopez',
-    role : 'Social Media Manager',
-    image : 'img/angela-lopez-social-media-manager.jpg'
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    image: "img/angela-lopez-social-media-manager.jpg",
   },
   {
-    name : 'Scott Estrada',
-    role : 'Developer',
-    image : 'img/scott-estrada-developer.jpg'
+    name: "Scott Estrada",
+    role: "Developer",
+    image: "img/scott-estrada-developer.jpg",
   },
   {
-    name : 'Barbara Ramos',
-    role : 'Graphic Designer',
-    image : 'img/barbara-ramos-graphic-designer.jpg'
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    image: "img/barbara-ramos-graphic-designer.jpg",
   },
-
 ];
 
 existingMember();
@@ -36,8 +35,8 @@ existingMember();
 console.log(givenMemberArr);
 console.log(memberArr);
 
-function existingMember(){
-  const container = document.querySelector('.team-container')
+function existingMember() {
+  const container = document.querySelector(".team-container");
 
   for (let index in givenMemberArr) {
     const member = givenMemberArr[index];
@@ -46,8 +45,7 @@ function existingMember(){
     let memberRole = member.role;
     let memberImage = member.image;
 
-    container.innerHTML += 
-    `
+    container.innerHTML += `
     <div class="team-card">
       <div class="card-image">
         <img src="${memberImage}"/>
@@ -57,47 +55,46 @@ function existingMember(){
         <p>${memberRole}</p>
       </div>
     </div>
-    `
+    `;
   }
 }
 
 // add new member on click (need fixing because on refresh it disappear)
 
-button.addEventListener('click', function(){
+button.addEventListener("click", function () {
   formInput();
 });
-button.addEventListener('click', () => {
-  inputs.forEach(input =>  input.value = '');
+button.addEventListener("click", () => {
+  inputs.forEach((input) => (input.value = ""));
 });
 
-function formInput (){
+function formInput() {
   storeMember();
   cardCreate();
-  function storeMember(){
-    let name = document.getElementById('name').value;
-    let role = document.getElementById('role').value;
-    let img = document.getElementById('image').value;
-  
+  function storeMember() {
+    let name = document.getElementById("name").value;
+    let role = document.getElementById("role").value;
+    let img = document.getElementById("image").value;
+
     const newMember = {
-      name : name,
-      role : role,
-      img : image
-    }
+      name: name,
+      role: role,
+      img: img,
+    };
     memberArr.push(newMember);
   }
 
-  function cardCreate(){
-    const container = document.querySelector('.team-container')
-  
+  function cardCreate() {
+    const container = document.querySelector(".team-container");
+
     for (let index in memberArr) {
       const member = memberArr[index];
-  
+
       let memberName = member.name;
       let memberRole = member.role;
       let memberImage = member.img;
-  
-      container.innerHTML += 
-      `
+
+      container.innerHTML += `
       <div class="team-card">
         <div class="card-image">
           <img src="${memberImage}"/>
@@ -107,9 +104,8 @@ function formInput (){
           <p>${memberRole}</p>
         </div>
       </div>
-      `
+      `;
       return member;
     }
   }
 }
-
